d59dca548ce75260fc456805ab82fce2ab96f5a2
Ana Karen Alvarez Palafox <alvarezpalafoxanakaren@gmail.com>
2023-06-08 14:33:21 -0600

    02 Variables

---
+++ variables.py
@@ -0,0 +1,15 @@
+#Python es NO tipado
+name = "Karen"
+age = 19
+is_single = False
+print(name)
+print(age)
+print(is_single)
+
+
+"""
+Hola, soy Karen, tengo 19 años y soltera = False
+"""
+#Utilizando format string
+print("Hola, soy {}, tengo {} y soltera = {}".
+format(name, age, is_single))
