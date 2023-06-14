2ceae5b767805d5eeb1d3a2fdba9f45756344dd6
Ana Karen Alvarez Palafox <alvarezpalafoxanakaren@gmail.com>
2023-06-14 13:45:17 -0600

    07 Strings

---
+++ strings.py
@@ -0,0 +1,29 @@
+my_string = "estE eS in TexTo"
+print(type(my_string))
+
+print(my_string.capitalize())
+print(my_string.upper())
+print(my_string.lower())
+print(my_string.title())
+
+print(len(my_string))
+
+#Indexing
+my_string = "Hola"
+
+print(my_string[3])
+for letter in my_string:
+    print(f"la letra{ letter }")
+
+    print(my_string[-1])
+    print(my_string[-2])
+
+    #Slicing
+    print(my_string[:])
+    print(my_string[0:3])
+    print(my_string[3:])
+    print(my_string[3:7])
+    print(my_string[:7])
+    print(my_string[::-1])
+    
+
